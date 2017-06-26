d3b29970c9b435a5fe98bd868f169f0cecec057a
Maxime Hersand <mhersand@clever-age.com>
2017-06-26 15:23:03 +0200

    remove svg

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 import  './app/fonts/robin_theme_fonts';
 import './dist/css/main.css';
-
-/* SVG */
-import svgXHR from './app/js/svgxhr';
-
-// require basic or custom sprite loader
-svgXHR({ path: './svg/**/*.svg', name: 'svg/main.svg' });
